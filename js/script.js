d3dd99972bd1cd301640ea72ba23f7118f428091
Paul Onyango <jorhnepaulone@gmail.com>
2020-03-02 09:54:56 +0300

    added readme and licence

--- js/script.js
+++ js/script.js
@@ -1,25 +1,27 @@
-var totalCosts = [];
 
-function order(size, crust, Topping) {
+//constractor
+function order(size, crust, topping) {
     this.size = size;
     this.crust = crust;
-    this.Topping = Topping;
+    this.topping = topping;
+
     this.price = 0;
 
 }
+//declaring
 var pizzaSize = ["Small", "Medium", "Large"];
 var pizzaCrust = ["Crispy", "Stuffed", "Gluten-free"];
-var pizzaTopping = ["Onion", "Sausage", "Becon",];
-
+var pizzaTopping = ["Onion", "Sausage", "Bacon"];
+//condition
 order.prototype.cost = function () {
     if (this.size === pizzaSize[0]) {
-        this.price += 500;
+        this.price += 599;
     }
     else if (this.size === pizzaSize[1]) {
-        this.price += 900;
+        this.price += 999;
     }
     else if (this.size === pizzaSize[2]) {
-        this.price += 1200;
+        this.price += 1099;
     }
 
     if (this.crust === pizzaCrust[0]) {
@@ -32,25 +34,27 @@ order.prototype.cost = function () {
         this.price += 200;
     }
 
-    if (this.Topping === pizzaTopping[0]) {
+    if (this.topping === pizzaTopping[0]) {
         this.price += 120;
     }
-    else if (this.Topping === pizzaTopping[1]) {
+    else if (this.topping === pizzaTopping[1]) {
         this.price += 160;
     }
-    else if (this.Topping === pizzaTopping[2]) {
+    else if (this.topping === pizzaTopping[2]) {
         this.price += 190;
     }
 
     return this.price;
 
-}
-order.prototype.totalCost = function () {
-    var orderTotal = 0;
-    for (var order = 0; order < totalCosts.length; order++) {
-        orderTotal += totalCosts[order];
+} 
+
+var finalCharge = [];
+order.prototype.total= function(){
+    var Total = 0;
+    for (var order = 0; order < finalCharge.length; order ++) {
+        Total += finalCharge[order];
     }
-    return orderTotal;
+    return Total;
 }
 
 $(document).ready(function () {
@@ -58,17 +62,17 @@ $(document).ready(function () {
         event.preventDefault();
         var sizes = $("select#size").val();
         var crusts = $("select#crust").val();
-        var Toppings = $("select#topping").val();
+        var toppings = $("select#topping").val();
 
-        var newPizzaOrder = new order(sizes, crusts, Toppings);
-        newPizzaOrder.cost();
-        totalCosts.push(newPizzaOrder.price);
+        var newOrder = new order(sizes, crusts, toppings);
+        newOrder.cost();
+        finalCharge.push(newOrder.price);
 
 
         $("#pz").text(sizes);
         $("#cr").text(crusts);
-        $("#to").text(Toppings);
-        $("#tc").text(newPizzaOrder.totalCost());
+        $("#to").text(toppings);
+        $("#tc").text(newOrder.total());
 
 
     });
